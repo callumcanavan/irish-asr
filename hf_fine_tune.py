e66057c4690a74786898be2c0a305e3e2e6a8c85
Callum Canavan <callumcanavan@gmail.com>
2024-01-26 17:58:55 +0000

    Start HF tutorial

--- hf_fine_tune.py
+++ hf_fine_tune.py
@@ -1,1 +1,16 @@
+from datasets import load_dataset, DatasetDict
 from huggingface_hub import notebook_login
+
+notebook_login()
+
+
+common_voice = DatasetDict()
+
+common_voice["train"] = load_dataset(
+    "mozilla-foundation/common_voice_13_0", "ir", split="train+validation"
+)
+common_voice["test"] = load_dataset(
+    "mozilla-foundation/common_voice_13_0", "ir", split="test"
+)
+
+print(common_voice)
